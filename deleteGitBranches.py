9f0801127f17530c3f791363971f472b0b93578d
Guilherme de Albuquerque <guilherme.albuquerque@way2.com.br>
2018-09-28 18:12:16 -0300

    already remove remote branches

--- deleteGitBranches.py
+++ deleteGitBranches.py
@@ -1,35 +1,33 @@
 from subprocess import check_output
 import sys, json, os
 
-
-def get_merged_branches():
+def getMergedBranches():
     ''' a list of merged branches, not couting the current branch or master '''
     raw_results = check_output('git branch --merged upstream/master', shell=True)
     return [b.strip() for b in raw_results.split('\n')
         if b.strip() and not b.startswith('*') and b.strip() != 'master']
 
-def load_json(filePath):
+def loadJson(filePath):
         with open(filePath) as json_data:
             jsonData = json.load(json_data)
             json_data.close()
             return jsonData
 
-def delete_branch(branch):
-    return check_output('git branch -D %s' % branch, shell=True).strip()
+def deleteBranch(branch):
+    return check_output('git push origin :{}'.format(branch), shell=True)
 
-
+def removeUnusedBranches(list_branches):
+    return [deleteBranch(branch) for branch in list_branches['values'] if list_branches['values']]
+        
 if __name__ == '__main__':
     path = os.path.dirname(os.path.abspath(__file__))
     config_json_path = os.path.join(path,'config.json').replace("\\","/")
-    load_json(config_json_path)
 
     dry_run = '--confirm' not in sys.argv
-    for branch in get_merged_branches():
-        if dry_run:
-            print(branch)
-        else:
-            print(delete_branch(branch))
     if dry_run:
         print('*****************************************************************')
         print('Did not actually delete anything yet, pass in --confirm to delete')
         print('*****************************************************************')
+    else:
+        removeUnusedBranches(loadJson(config_json_path))
+       
